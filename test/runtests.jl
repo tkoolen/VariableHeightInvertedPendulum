93308ba8a078d567185d6d1e609b282fb09b5214
Twan Koolen <koolen.twan@gmail.com>
2016-10-27 19:16:40 -0400

    test notebooks, run on travis

---
+++ test/runtests.jl
@@ -0,0 +1,19 @@
+using Base.Test
+
+# notebooks
+@testset "example notebooks" begin
+    using IJulia
+
+    outputdir = joinpath(tempdir(), "VariableHeightInvertedPendulum")
+    if !isdir(outputdir)
+        mkpath(outputdir)
+    end
+    jupyter = IJulia.jupyter
+    notebookdir = joinpath("..", "notebook")
+    for f in filter(x -> endswith(x, "ipynb"), readdir(notebookdir))
+        notebook = joinpath(notebookdir, f)
+        output = joinpath(outputdir, f)
+        # skip on nightly because notebooks specify version 0.5
+        @test begin run(`$jupyter nbconvert --to notebook --execute $notebook --output $output`); true end
+    end
+end
