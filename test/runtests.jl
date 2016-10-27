7a748f2bddfb56b278db75be27481eee49e3d219
Twan Koolen <koolen.twan@gmail.com>
2016-10-27 19:52:26 -0400

    increase timeout

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,6 @@ using Base.Test
         notebook = joinpath(notebookdir, f)
         output = joinpath(outputdir, f)
         # skip on nightly because notebooks specify version 0.5
-        @test begin run(`$jupyter nbconvert --to notebook --execute $notebook --output $output`); true end
+        @test begin run(`$jupyter nbconvert --to notebook --execute $notebook --output $output --ExecutePreprocessor.timeout=90`); true end
     end
 end
